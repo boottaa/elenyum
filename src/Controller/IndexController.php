508b1980cfb2a03cda7647779cd4851638ef16a8
bootta <Bootta@yandex.ru>
2022-01-07 00:30:15 +0300

    add employee

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,19 +28,29 @@ class IndexController extends AbstractController
         return $this->render('index/calendar.html.twig', []);
     }
 
-    #[Route('/registration', name: 'appRegistration')]
+    #[Route('/employee/add', name: 'employeeAdd')]
+    public function employeeAdd(): Response
+    {
+        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('appLogin');
+        }
+
+        return $this->render('index/calendar.html.twig', []);
+    }
+
+    #[Route('/registration', name: 'registration')]
     public function registration(): Response
     {
         return $this->render('index/registration.html.twig', []);
     }
 
-    #[Route('/login', name: 'appLogin')]
+    #[Route('/login', name: 'login')]
     public function login(): Response
     {
         return $this->render('index/login.html.twig', []);
     }
 
-    #[Route('/forgotPassword', name: 'appForgotPassword')]
+    #[Route('/forgotPassword', name: 'forgotPassword')]
     public function forgotPassword(): Response
     {
         return $this->render('index/forgotPassword.html.twig', []);
