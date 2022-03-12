0d4ba34ff0548b793acd0ef5fc95399c4e658ec2
bootta <Bootta@yandex.ru>
2022-03-12 15:10:52 +0300

    del set password in hydrate password

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
 use App\Entity\Employee;
 use App\Entity\Role;
 use App\Exception\ArrayException;
 use App\Service\ClientService;
+use App\Service\EmployeeService;
+use App\Validator\EmployeeValidator;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,7 +23,7 @@ class ClientController extends AbstractController
      * @return Response
      * @throws \App\Exception\ArrayException
      */
-    #[Route('/api/client/query', name: 'client')]
+    #[Route('/api/client/query', name: 'apiClientQuery')]
     public function query(ClientService $service, Request $request): Response
     {
         if (! $this->isGranted(Role::ROLE_SHEDULE_ALL) && ! $this->isGranted(Role::ROLE_SHEDULE_ME)) {
@@ -41,6 +45,147 @@ class ClientController extends AbstractController
             'total' => $list->getNumResults(),
             'page' => $list->getCurrentPage(),
             'size' => $list->getPageSize(),
+        ]);
+    }
+
+    /**
+     * @param ClientService $service
+     * @param Request $request
+     * @return Response
+     * @throws ArrayException
+     */
+    #[IsGranted(Role::ROLE_SHEDULE_ALL)]
+    #[Route('/api/client/list', name: 'apiClientList')]
+    public function list(ClientService $service, Request $request): Response
+    {
+        $page = $request->get('page', 1);
+        $user = $this->getUser();
+        if (!$user instanceof Employee) {
+            return $this->json((new ArrayException('Пользователь не найден', 202))->toArray());
+        }
+        $list = $service->list(['company' => $user->getCompany()], $page);
+
+        return $this->json([
+            'success' => true,
+            'items' => $list->getResults(),
+            'total' => $list->getNumResults(),
+            'page' => $list->getCurrentPage(),
+            'size' => $list->getPageSize(),
+        ]);
+    }
+
+    #[IsGranted(Role::ROLE_SHEDULE_ALL)]
+    #[Route('/api/client/get/{clientId<\d+>}', name: 'apiClientGet', methods: 'GET')]
+    public function getClient(int $clientId, EmployeeService $service): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof Employee) {
+            return $this->json((new ArrayException('Пользователь не найден', 202))->toArray());
+        }
+
+        return $this->json([
+            'success' => true,
+            'item' => $service->get($clientId),
+        ]);
+    }
+
+    /**
+     * @param int $clientId
+     * @param ClientService $service
+     * @return Response
+     */
+    #[IsGranted(Role::ROLE_SHEDULE_ALL)]
+    #[Route('/api/client/delete/{clientId<\d+>}', name: 'apiClientDelete', methods: 'DELETE')]
+    public function delete(int $clientId, ClientService $service): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof Client) {
+            return $this->json((new ArrayException('Пользователь не найден', 202))->toArray());
+        }
+
+        try {
+            $service->del($clientId);
+        } catch (ArrayException $arrayException) {
+            return $this->json($arrayException);
+        }
+
+        return $this->json([
+            'success' => true,
+        ]);
+    }
+
+    /**
+     * Create resource
+     *
+     * @param Request $request
+     * @param ClientService $service
+     * @param EmployeeValidator $validator
+     * @return Response
+     * @throws ArrayException
+     * @throws \JsonException
+     */
+    #[IsGranted(Role::ROLE_SHEDULE_ALL)]
+    #[Route('/api/client/post', name: 'apiClientPost', methods: 'POST')]
+    public function post(Request $request, ClientService $service, EmployeeValidator $validator): Response
+    {
+        $content = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        if ($validator->isValid($content)) {
+            $user = $this->getUser();
+            if (!$user instanceof Client) {
+                return $this->json((new ArrayException('Пользователь не найден', 202))->toArray());
+            }
+            $data['client'] = $content;
+            $data['user'] = $user;
+
+            $client = $service->post($data);
+
+            return $this->json([
+                'success' => true,
+                'item' => $client
+            ]);
+        }
+
+        return $this->json([
+            'success' => false,
+            'message' => 'Не корректные данные',
+        ]);
+    }
+
+    /**
+     * Update resource
+     *
+     * @param Request $request
+     * @param EmployeeService $service
+     * @param EmployeeValidator $validator
+     * @return Response
+     * @throws ArrayException
+     * @throws \JsonException
+     */
+    #[IsGranted(Role::ROLE_EMPLOYEE_EDIT)]
+    #[Route('/api/client/put/{clientId<\d+>?}', name: 'apiEmployeePut', methods: 'PUT')]
+    public function put(Request $request, EmployeeService $service, EmployeeValidator $validator): Response
+    {
+        $content = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+
+        if ($validator->isValid($content)) {
+            $user = $this->getUser();
+            if (!$user instanceof Employee) {
+                return $this->json((new ArrayException('Пользователь не найден', 202))->toArray());
+            }
+            $data['employee'] = $content;
+
+            $employee = $service->put($data);
+
+            return $this->json([
+                'success' => true,
+                'item' => $employee,
+            ]);
+        }
+
+        return $this->json([
+            'success' => false,
+            'message' => 'Не корректные данные',
+            'errors' => $validator->getErrors()
         ]);
     }
 }
--- src/Service/ClientService.php
+++ src/Service/ClientService.php
@@ -2,6 +2,9 @@
 
 namespace App\Service;
 
+use App\Entity\Client;
+use App\Entity\Employee;
+use App\Exception\ArrayException;
 use App\Repository\ClientRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -12,5 +15,78 @@ class ClientService extends BaseAbstractService
         private EntityManagerInterface $em
     ) {
         $this->repository = $repository;
+    }
+
+    /**
+     * @param Client $client
+     * @param array $data
+     * @return void
+     */
+    private function hydrate(Client $client, array $data): void
+    {
+        $client->setName($data['name']);
+        $client->setDateBrith($data['dateBrith']);
+        $client->setStatus($data['status']);
+        $client->setEmail($data['email']);
+        $client->setPhone($data['phone']);
+        $client->setAdditionalPhone($data['additionalPhone']);
+    }
+
+    /**
+     * @param array $data
+     * @return Client
+     * @throws ArrayException
+     */
+    public function put(array $data): Client
+    {
+        $clientData = $data['client'];
+        $client = $this->repository->find($clientData['id']);
+        if (!$client instanceof Client) {
+            throw new ArrayException('Not defined '.Client::class, '422');
+        }
+        $this->hydrate($client, $clientData);
+        $this->em->flush();
+
+        return $client;
+    }
+
+    /**
+     * @param array $data
+     * @return Client
+     * @throws ArrayException
+     */
+    public function post(array $data): Client
+    {
+        $user = $data['user'];
+        $clientData = $data['client'];
+
+        if (!$user instanceof Employee) {
+            throw new ArrayException('Not defined '.Employee::class, '422');
+        }
+
+        $client = new Client();
+        $client->setCompany($user->getCompany());
+        $this->hydrate($client, $clientData);
+        $this->em->flush();
+
+        return $client;
+    }
+
+    /**
+     * @param int $id
+     * @return bool
+     * @throws ArrayException
+     */
+    public function del(int $id): bool
+    {
+        $client = $this->repository->find($id);
+        if (!$client instanceof Client) {
+            throw new ArrayException('Not defined '.Client::class, '422');
+        }
+
+        $this->em->remove($client);
+        $this->em->flush();
+
+        return true;
     }
 }
