7b40063686b345cc7b04f072d1f9c0ee20c7abe5
bootta <Bootta@yandex.ru>
2022-03-25 23:03:26 +0300

    fixed

--- assets/forgotPassword.js
+++ assets/forgotPassword.js
@@ -1,10 +1,39 @@
 import './bootstrap';
 import Vue from 'vue';
 import {isValid} from "./validator/validator";
+import {post} from "./src/baseQuery";
 
 let object = {
     email: null,
 };
+
+let vueModal = new Vue({
+    el: '#modalAlert',
+    data() {
+        return {
+            elModal: null,
+            modal: null,
+            message: '',
+        }
+    },
+    mounted() {
+        this.$root.$on('show', function () {
+            this.show()
+        });
+    },
+    methods: {
+        show() {
+            this.elModal = document.getElementById('modalAlert');
+            this.modal = new bootstrap.Modal(this.elModal);
+            this.modal.show();
+
+            this.elModal.addEventListener('hidden.bs.modal', function (e) {
+                location.href = '/login';
+            });
+        }
+    },
+    delimiters: ['${', '}$'],
+});
 
 new Vue({
     el: '#forgotPassword',
@@ -23,7 +52,7 @@ new Vue({
             let items = {
                 '#email': {
                     value: this.object.email,
-                    validators: ['notEmpty'],
+                    validators: ['notEmpty', 'email'],
                 },
             };
             return isValid(items);
@@ -31,8 +60,19 @@ new Vue({
         send() {
             if (this.validation()) {
 
-                console.log(this.object)
-                this.resetObject();
+                post('/api/forgotPassword', data, (result) => {
+                    if (result.success === true) {
+                        vueModal.message = result.message;
+                        vueModal.$root.$emit('show');
+                        vueModal.$once('hidden', () => {
+                            this.resetObject();
+                            window.location.href = '/login';
+                        });
+                    } else {
+                        vueModal.message = result.message;
+                        vueModal.$root.$emit('show');
+                    }
+                });
             }
         },
 
